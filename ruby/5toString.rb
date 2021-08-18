55f85c9847588b593b0178c537ad7c42ab937842
Raskolnikov <jerry.tann@opteyes.cn>
2021-08-18 21:20:11 +0800

    feat: finish strings, start hash

--- ruby/5toString.rb
+++ ruby/5toString.rb
@@ -1,1 +1,28 @@
+
+# string documentation https://ruby-doc.org/core-3.0.2/String.html
+
 puts 40.to_s.reverse
+
+str = "hello world goodbye world"
+
+puts str 
+
+subbed = str.gsub("world", "cruel world")
+
+puts subbed 
+
+replaced = subbed.reverse
+
+puts replaced
+
+lineByLine = "howdy world\nnew line work?"
+
+puts lineByLine
+
+puts lineByLine.lines.reverse
+
+puts lineByLine.lines.reverse.join
+
+capitalized = replaced.swapcase 
+
+puts capitalized
