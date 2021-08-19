f8760acd882c3e090540b0a3bcad7d38596aedee
Raskolnikov <jerry.tann@opteyes.cn>
2021-08-20 07:54:55 +0800

    feat: map item counting method

--- ruby/11json.rb
+++ ruby/11json.rb
@@ -16,3 +16,27 @@ s["root"].each { |key, val|
     puts val["title"]
 }
 
+def getS() 
+  s= { 
+    "root" => {
+        "level1" => {
+            'title' => "A"
+        },
+        "level1b" => {
+            "title" => "B"
+        }
+    }
+  }
+  return s
+end
+
+def countItemsInRoot()
+    s = getS
+  s["root"].each { |key, val|
+    puts key
+    puts val 
+  }
+  return s["root"].count
+end 
+
+puts countItemsInRoot
