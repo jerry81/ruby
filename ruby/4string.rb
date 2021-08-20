7feb1ee394fa25df78b59c4e2876b7b114e58f5d
Raskolnikov <jerry.tann@opteyes.cn>
2021-08-20 16:41:03 +0800

    feat: template strings

--- ruby/4string.rb
+++ ruby/4string.rb
@@ -3,3 +3,13 @@ puts "Jerry".reverse
 puts "Jerry".length
 
 puts "Jerry"*5 
+
+# template str 
+
+a = "abcdefghijklmnop"
+
+b = "203958105"
+
+puts "#{a} #{b}"
+
+puts "justified is #{a.ljust(30)} #{b}"
