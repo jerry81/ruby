f4dbf93eb0f4f2d6d01858c6b49217817ef00606
Raskolnikov <jerry.tann@opteyes.cn>
2021-08-21 21:09:59 +0800

    feat: oo - learned there is no automatic hoisting of functions

--- ruby/13oo.rb
+++ ruby/13oo.rb
@@ -1,13 +1,31 @@
 class Cat # capital letter
     attr_accessor :breed, :name, :poopSize
-    def initialize
-        
+    def initialize(breed="", name="",poopSize=:M)
+        @breed = breed# like this.breed
+        @name = name[1..10]
+        @poopSize = poopSize
     end
 end
 
 c1 = Cat.new
 c1.breed = "garfield"
 c1.name = "cat mcCatFace"
-c1.poopSize = "XL"
+c1.poopSize = :XL
 
-puts c1
+# must be defined before use 
+
+def printCat(cat)
+    puts cat.name 
+    puts cat.breed 
+    puts cat.poopSize 
+end
+
+printCat(c1)
+
+defaultCat = Cat.new
+printCat(defaultCat)
+
+initializedCat = Cat.new("american shorthair", "chester", :S)
+printCat(initializedCat)
+
+
